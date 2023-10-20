7eb04b1610f6576a418143ae3f2776cc0fed1929
jihadboussata <jihadboussata@gmail.com>
2023-10-20 11:07:01 -0700

    what the hell is this

--- _printf.c
+++ _printf.c
@@ -1,45 +1,52 @@
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <unistd.h>
 #include "main.h"
-/**
- * _printf - is a function that selects the correct function to print.
- * @format: identifier to look for.
- * Return: the length of the string.
- */
-int _printf(const char * const format, ...)
-{
-	convert_match m[] = {
-		{"%s", printf_string}, {"%c", printf_char},
-		{"%%", printf_37},
-		{"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
-		{"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
-		{"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX},
-		{"%S", printf_exclusive_string}, {"%p", printf_pointer}
-	};
 
+/**
+ * _printf - produces output according to a format
+ * @format: The specified format
+ *
+ * Return: The number of characters that were printed
+ */
+int _printf(const char *format, ...)
+{
+	int  i = 0, k = 0;
+	int n_displayed = 0;
+	char *str = NULL;
 	va_list args;
-	int i = 0, j, len = 0;
+	int (*func)(va_list);
 
 	va_start(args, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
 
-Here:
 	while (format[i] != '\0')
 	{
-		j = 13;
-		while (j >= 0)
+		if (format[i] != '%')
 		{
-			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
-			{
-				len += m[j].f(args);
-				i = i + 2;
-				goto Here;
-			}
-			j--;
+			_putchar(format[i]);
+			n_displayed++;
 		}
-		_putchar(format[i]);
-		len++;
+		else if (format[i + 1] == '%')
+		{
+			i++;
+			_putchar('%');
+			n_displayed++;
+		}
+		else
+		{
+			func = _selec_func(format[i + 1]);
+			if (func != NULL)
+			{
+				n_displayed += func(args);
+				i++;
+			}
+		}
+
 		i++;
 	}
+
 	va_end(args);
-	return (len);
+	return (n_displayed);
 }
